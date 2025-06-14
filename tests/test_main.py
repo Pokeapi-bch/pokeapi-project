4dc1acc49364286264a88c1166db5c7f3e6f49ce
BastianChamblas <b.chamblas@duocuc.cl>
2025-06-14 09:05:02 -0400

    Aplicando cambios pre-commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,21 @@
 from fastapi.testclient import TestClient
+
 from main import app
 
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert "html" in response.text
 
+
 def test_get_pokemon_valid():
     response = client.get("/pokemon/pikachu")
     assert response.status_code == 200
     assert response.json()["name"] == "pikachu"
+
 
 def test_get_pokemon_invalid():
     response = client.get("/pokemon/invalidpokemon")
